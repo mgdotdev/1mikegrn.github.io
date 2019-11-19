4deecf77c6602011175de134ffe3fa48205566d4
1mikegrn <1mikegrn@gmail.com>
2019-11-19 17:00:56 -0600

    added pyGC and libRL websites in the pages, updated html structure to use js for repetitive html. Changed custom button layouts.

---
+++ js/drop_down.js
@@ -0,0 +1,9 @@
+document.write(`
+<div class="dropdown-menu">
+    <a class="dropdown-item" href="https://github.com/1mikegrn">Github Profile</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="https://1mikegrn.github.io/pages/libRL/">libRL</a>
+    <a class="dropdown-item" href="https://1mikegrn.github.io/pages/CompGen/">CompGen</a>
+    <a class="dropdown-item" href="https://1mikegrn.github.io/pages/pyGC/">pyGC</a>
+</div>
+`);
---
+++ js/page_tag.js
@@ -0,0 +1,42 @@
+document.write(`
+    <!-- Contact -->
+    <section id="contact" class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 headline">
+                    <h2>Contact</h2>
+                </div>
+                <div class="col-md-6">
+                    <a href="https://1mikegrn.github.io/docs/Resume%20-%20Michael%20Green.pdf" class="btn btn-default btn-custom-2">Download Resume</a>
+                    <a href="https://1mikegrn.github.io/docs/Curriculum%20Vitae%20-%20Michael%20Green.pdf" class="btn btn-default btn-custom-2">Download CV</a>
+                    <ul class="icon-list">
+                        <li><br>Flarsheim Hall Rm 320, University of Missouri-Kansas City, MO, USA</li>
+                        <li><a href="mailto:michael.green@mail.umkc.edu">michael.green@mail.umkc.edu</a></li>
+                        <li><a href="">https://1mikegrn.github.io</a></li>
+                        <li>+1 (907) 570-1506</li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <h3>About</h3>
+                    <p align="justify">
+                        Michael Green is a PhD candidate under the supervision of Dr. Xiaobo Chen with the Department of Chemistry at the University of Missouri–Kansas City. He received his Bachelors of Science in chemistry with a minor in mathematics from the University of Idaho in 2016, and his Masters of Science in chemistry from the University of Missouri–Kansas City in 2019. His research interests include the development, characterization, modeling, and application of nanomaterials in light/matter interactions, focusing on photolysis, photocatalysis, and microwave absorption, as well as short-range matter/matter interactions with a focus in physical adsorption.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact -->
+
+    <!-- Footer -->
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="copy">
+                        © 2019 Michael Green
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer -->`);
