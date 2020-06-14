adb800fc4fc3b43c62fcde1ff8305c6fbb98316a
1mikegrn <1mikegrn@gmail.com>
2020-06-14 13:22:04 -0700

    refactored to use portfolio

--- js/drop_down.js
+++ js/drop_down.js
@@ -2,6 +2,6 @@ document.write(`
 <div class="dropdown-menu">
     <a class="dropdown-item" href="https://github.com/1mikegrn">Github Profile</a>
     <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="https://1mikegrn.github.io/pages/">Portfolio</a>
+    <a class="dropdown-item" href="https://1mikegrn.github.io/portfolio/">Portfolio</a>
 </div>
 `);
