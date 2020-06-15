60c2565858e81a14ab7229ebc5f4d1046f33a961
1mikegrn <1mikegrn@gmail.com>
2020-06-15 06:54:35 -0700

    update pages menu

--- js/drop_down.js
+++ js/drop_down.js
@@ -1,4 +1,8 @@
 document.write(`
+<a class="git-button nav-link dropdown-toggle" role="button" data-toggle="dropdown" id="navbarDropdown"
+aria-haspopup="true" href="#">
+Pages
+</a>
 <div class="dropdown-menu">
     <a class="dropdown-item" href="https://github.com/1mikegrn">Github Profile</a>
     <div class="dropdown-divider"></div>
