2e6e958c74786929866e30e560736269df17f160
Michael Green <michaelgrn@LAPTOP-ML66RQ8U.localdomain>
2020-10-17 16:02:48 -0700

    added content, changed to dark mode

--- js/page_tag.js
+++ js/page_tag.js
@@ -10,8 +10,7 @@ document.write(`
                     <a href="https://1mikegrn.github.io/docs/Resume%20-%20Michael%20Green.pdf" class="btn btn-default btn-custom-2">Download Resume</a>
                     <a href="https://1mikegrn.github.io/docs/Curriculum%20Vitae%20-%20Michael%20Green.pdf" class="btn btn-default btn-custom-2">Download CV</a>
                     <ul class="icon-list">
-                        <li><br>Flarsheim Hall Rm 320, <br>University of Missouri-Kansas City, MO, USA</li>
-                        <li><a href="mailto:magwwc@umsystem.edu">magwwc@umsystem.edu</a></li>
+                        <li><br><a href="mailto:1mikegrn@gmail.com">1mikegrn@gmail.com</a></li>
                         <li><a href="https://1mikegrn.github.io">https://1mikegrn.github.io</a></li>
                         <li>+1 (907) 570-1506</li>
                     </ul>
